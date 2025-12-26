8086fccbe5bc6515781e13da786e512dfa5fc7f4
aswanth-mc <mcaswanth5@gmail.com>
2025-12-26 15:52:47 +0530

    add login function

--- menu.py
+++ menu.py
@@ -31,15 +31,39 @@ insert into users (username, password, phone, role)
 values ('waiter1', 'waiter123', '1122334455', 'waiter')
 ''')
 
+def login():
+    phone = input ("enter phone number: ")
+    password = input("enter password: ")
+    cursor.execute('''
+    select username,role from users where phone = ? and password = ?''', (phone, password))
+
+    user = cursor.fetchone()
+
+    if user:
+        username,role = user
+        if role == 'manager':
+            print(f"welcome manager {username}")
+        elif role == 'cheif':
+            print(f"welcome cheif {username}")
+        elif role == 'waiter':
+            print(f"welcome waiter {username}")
+        else:
+            print(f"welcome {username}")
+    else:
+        print("invalid phone number or password")
+
 while True:
     print("\nWelcome to the Restaurant Menu Management System")
     print("1. user registration")
+    print("2. login")
     print("0. Exit")
 
     choice = input("enter your choice: ")
 
     if choice == "1":
         print("user registration")
+    elif choice == "2":
+        login()
     elif choice == "0":
         break
     else:
