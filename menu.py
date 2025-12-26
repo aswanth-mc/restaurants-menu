08a07b1d4ea6f7a3fa3b5de0a1f0b04533e95b70
aswanth-mc <mcaswanth5@gmail.com>
2025-12-26 19:49:51 +0530

    enhance login functionality and implement order management features

--- menu.py
+++ menu.py
@@ -35,12 +35,12 @@ def login():
     phone = input ("enter phone number: ")
     password = input("enter password: ")
     cursor.execute('''
-    select username,role from users where phone = ? and password = ?''', (phone, password))
+    select id,username,role from users where phone = ? and password = ?''', (phone, password))
 
     user = cursor.fetchone()
 
     if user:
-        username,role, = user
+        id,username,role = user
         if role == 'manager':
             print(f"\nwelcome manager {username}")
         elif role == 'cheif':
@@ -50,7 +50,7 @@ def login():
             print(f"\nwelcome waiter {username}")
         else:
             print(f"\nwelcome {username}")
-            customer()
+            customer(id)
     else:
         print("invalid phone number or password")
 
@@ -105,13 +105,51 @@ def view_menu():
         availability_status = "Available" if availability == 1 else "Not Available"
         print(f"ID: {id}, Name: {item_name}, Category: {category}, Price: {price}, Availability: {availability_status}")
 
+#order table
+cursor.execute('''
+               create table if not exists orders (
+               id integer primary key autoincrement,
+               customer_id integer not null,
+               menu_item_id integer not null,
+               quantity integer not null,
+               table_number integer not null,
+               foreign key (customer_id) references users(id),
+               foreign key (menu_item_id) references menu(id))  ''')
+# order placeing
+def place_order(customer_id):
+    menu_item_id = int(input("Enter menu item id: "))
+    quantity = int(input("Enter quantity: "))
+    table_number = int(input("Enter table number: "))
 
+    cursor.execute('''
+    insert into orders (customer_id, menu_item_id, quantity, table_number)
+    values (?, ?, ?, ?)
+    ''', (customer_id, menu_item_id, quantity, table_number))
+
+    conn.commit()
+    print("Order placed successfully")
+
+#view order
+def view_orders():
+    cursor.execute('''
+    select o.id, u.username, m.item_name, o.quantity, o.table_number
+    from orders o
+    join users u on o.customer_id = u.id
+    join menu m on o.menu_item_id = m.id
+    ''')
+    orders = cursor.fetchall()
+
+    print("\nOrders:")
+    for order in orders:
+        id, username, item_name, quantity, table_number = order
+        print(f" Item: {item_name}, Quantity: {quantity}, Table Number: {table_number}")
 
 # chef function
 def cheif():
     while True:
-        print("1.add menu item")
+        print("\n1.add menu item")
         print("2.view menu")
+        print("3.view orders")
         print("0.logout")
 
         choice = input("enter your choice: ")
@@ -119,26 +157,33 @@ def cheif():
             add_menu()
         elif choice == "2":
             view_menu()
+        elif choice == "3":
+            view_orders()
         elif choice == "0":
             break
         else:
             print("invalid choice, please try again.")
 
 # customer function
-def customer():
+def customer(customer_id):
     while True:
-        print("1.view menu")
-        print("0.logout")
+        print("\n1. View Menu")
+        print("2. Place Order")
+        print("0. Logout")
 
         choice = input("enter your choice: ")
+
         if choice == "1":
             view_menu()
+        elif choice == "2":
+            place_order(customer_id)
         elif choice == "0":
             break
         else:
-            print("invalid choice, please try again.")
+            print("invalid choice")
 
 
+# main
 while True:
     print("\nWelcome to the Restaurant Menu Management System")
     print("1. user registration")
